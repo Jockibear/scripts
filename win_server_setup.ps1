eb2af5afc1ebeaea2ad6f3ce5d37524c0b20e25b
Jockibear <janik.lueders@gmail.com>
2026-02-19 14:47:55 +0100

    added tcp-reflect script

--- win_server_setup.ps1
+++ win_server_setup.ps1
@@ -1,4 +1,4 @@
-#variablen: $hostname, $primary_dns, $secondary_dns, $domain, $ipaddress
+#variablen: $hostname, $primary_dns, $secondary_dns, $domain, $ipaddress, $admin_server
 # $local_user = Get-LocalGroupMember -Group "Administrators"
 Rename-Computer -NewName $hostname
 Get-NetIPInterface
@@ -6,5 +6,7 @@ set-netipaddress -interfaceindex 6 -IPAddress $ipaddress
 Set-DnsClientServerAddress -InterfaceIndex 6 -ServerAddresses ($primary_dns, $secondary_dns)
 Restart-Computer -Force
 Add-Computer -ComputerName $hostname -LocalCredential $hostname\Administrator -DomainName $domain -Credential $domain\Administrator -Force
-Enable-PSremoting -force
+Enable-PSremoting -Force
+Set-Item wsman:\localhost\client\trustedhosts $admin_server
+Install-WindowsFeature -Name RSAT-AD-Tools -IncludeManagementTools
 Restart-Computer -Force

